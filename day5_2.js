2cbea3e9e9043f328c22e278d1311a578c15a915
Jasper Vercammen <jasper.vercammen@icapps.com>
2017-12-05 16:43:50 +0100

    Solution 2, day 5

---
+++ day5_2.js
@@ -0,0 +1,26 @@
+fs = require('fs');
+
+const getInput = () => {
+  return new Promise(resolve => {
+    fs.readFile('./day5_input.txt', 'utf8', (err, data) => {
+      resolve(data);
+    });
+  });
+};
+
+const main = async () => {
+  const input = await getInput();
+  const maze = input.split('\n').map(Number);
+  let index = 0;
+  let count = 0;
+  while (maze[index] !== undefined) {
+    const tmpIndex = index;
+    index = maze[index] + index;
+    maze[tmpIndex] += maze[tmpIndex] >= 3 ? -1 : 1;
+    count++;
+  }
+  const solution = count;
+  return solution;
+};
+
+main().then(res => console.log(res));
