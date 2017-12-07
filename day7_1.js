0a57c23f6c7980dca5edf4ba3672d1d8fbfc3d54
Jasper Vercammen <jasper.vercammen@icapps.com>
2017-12-07 19:30:49 +0100

    Solution 1, day 7

--- day7_1.js
+++ day7_1.js
@@ -1,7 +1,20 @@
-const input = ``;
+fs = require('fs');
+
+const getInput = () => {
+  return new Promise(resolve => {
+    fs.readFile('./day7_input.txt', 'utf8', (err, data) => {
+      resolve(data);
+    });
+  });
+};
 
 const main = async () => {
-  const solution = 1;
+  const input = await getInput();
+  const regex = /[a-z]{4,8}/g;
+  const programs = input.match(regex);
+  // If we filter every duplicte occurence, there will be only one root program left
+  const topProgram = programs.filter(m => programs.lastIndexOf(m) === programs.indexOf(m));
+  const solution = topProgram[0];
   return solution;
 };
 
